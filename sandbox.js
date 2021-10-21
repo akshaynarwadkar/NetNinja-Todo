33ff45cf0ae323f3cc5f15f27f139109970b2909
akshaynarwadkar <90238392+akshaynarwadkar@users.noreply.github.com>
2021-10-21 09:03:42 +0530

    Initial commit

---
+++ sandbox.js
@@ -0,0 +1,30 @@
+const ul = document.querySelector('ul');
+const button = document.querySelector('button')
+
+
+
+button.addEventListener('click',()=>{
+	const li=document.createElement('li')
+	li.textContent=prompt('Add an item')
+	ul.append(li)
+	console.log(ul)
+
+	newUl = Array.from(ul.children)
+	newUl.forEach(item => {
+		item.addEventListener('click',e => {
+			e.target.remove()
+		})
+	});
+})
+
+
+
+// ul.addEventListener('click',()=>{
+// 	console.log(ul)
+// })
+
+
+
+
+
+
